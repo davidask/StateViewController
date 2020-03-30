a65cc0c40010a233a45f5e1cd0c67642b2c47f9c
David Ask <david@formbound.com>
2020-03-30 18:20:43 +0200

    Updates

--- Sources/StateViewControllerTransitioning.swift
+++ Sources/StateViewControllerTransitioning.swift
@@ -6,56 +6,62 @@ import UIKit
 
 /// View controllers can conform to this protocol to provide their desired
 /// state transitioning behaviour when contained in a `StateViewController`.
+@objc
 public protocol StateViewControllerTransitioning: AnyObject {
 
     /// Returns the animation duration for a state transition of this view controller.
     ///
     /// - Parameter isAppearing: Whether this view controller is appearing.
     /// - Returns: A transition duration.
+    @objc
     func stateTransitionDuration(isAppearing: Bool) -> TimeInterval
 
     /// Notifies that a state transition will begin for this view controller.
     ///
     /// - Parameter isAppearing: Whether this view controller is appearing.
+    @objc
     func stateTransitionWillBegin(isAppearing: Bool)
 
     /// Notifies that a state transition did end for this view controller.
     ///
     /// - Parameter isAppearing: Whether this view controller is appearing.
+    @objc
     func stateTransitionDidEnd(isAppearing: Bool)
 
     /// Animations performed alongside the state transition of this view controller.
     ///
     /// - Parameter isAppearing: Whether this view controller is appearing.
+    @objc
     func animateAlongsideStateTransition(isAppearing: Bool)
 
     /// Returns the animation delay for a state transition of the provided view controller.
     ///
     /// - Parameter isAppearing: Whether this view controller is appearing.
     /// - Returns: A transition duration.
+    @objc
     func stateTransitionDelay(isAppearing: Bool) -> TimeInterval
 }
 
 #if canImport(AppKit)
 extension NSViewController: StateViewControllerTransitioning {
 
-    public func stateTransitionDuration(isAppearing: Bool) -> TimeInterval {
-        0.5
+    open func stateTransitionDuration(isAppearing: Bool) -> TimeInterval {
+        3.5
     }
 
-    public func stateTransitionWillBegin(isAppearing: Bool) {
+    open func stateTransitionWillBegin(isAppearing: Bool) {
         view.alphaValue = isAppearing ? 0 : 1
     }
 
-    public func stateTransitionDidEnd(isAppearing: Bool) {
+    open func stateTransitionDidEnd(isAppearing: Bool) {
         view.alphaValue = 1
     }
 
-    public func animateAlongsideStateTransition(isAppearing: Bool) {
+    open func animateAlongsideStateTransition(isAppearing: Bool) {
         view.animator().alphaValue = isAppearing ? 1 : 0
     }
 
-    public func stateTransitionDelay(isAppearing: Bool) -> TimeInterval {
+    open func stateTransitionDelay(isAppearing: Bool) -> TimeInterval {
         0
     }
 
